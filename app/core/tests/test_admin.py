60d66b2bec4693c2839883d6a9bf17a565fba8e5
Chris Cho <chrisycho04@gmail.com>
2024-04-11 00:07:32 -0700

    setting up django admin and tests

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,48 @@
+"""
+Tests for Django Admin modifications
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    """ Tests for Django Admin """
+
+    def setUp(self):
+        """ create user and client """
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@example.com",
+            password="testpw123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="user@example.com",
+            password="testpw123",
+            name='Test USer'
+        )
+
+    def test_user_list(self):
+        """ Tests that users are listed on the page """
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """ Test the user edit page works """
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """ test create user page works """
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
