1e04da463a87f36179e7cfbd26c91988fd1581d7
Chris Cho <chrisycho04@gmail.com>
2024-04-12 00:21:07 -0700

    Added token API and tests

--- app/user/views.py
+++ app/user/views.py
@@ -3,9 +3,20 @@ Views for the user API
 """
 
 from rest_framework import generics  # type: ignore
-from user.serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken  # type: ignore
+from rest_framework.settings import api_settings  # type: ignore
+
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     """ Create a new user in the system """
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
